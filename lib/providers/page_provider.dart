ac85984579a10f7cddebe9dc3caf779704528315
Fernando Herrera <fernando.herrera85@gmail.com>
2021-04-26 10:27:53 -0400

    Fin sección 5

--- lib/providers/page_provider.dart
+++ lib/providers/page_provider.dart
@@ -8,15 +8,22 @@ class PageProvider extends ChangeNotifier {
   PageController scrollController = new PageController();
 
   List<String> _pages = ['home','about','pricing','contact','location'];
+  int _currentIndex = 0;
 
 
   createScrollController( String routeName ) {
     this.scrollController = new PageController( initialPage: getPageIndex(routeName) );
+    
+    html.document.title = _pages[ getPageIndex(routeName) ];
 
     this.scrollController.addListener(() {
       final index = (this.scrollController.page ?? 0).round();
       
-      html.window.history.pushState(null, 'none', '#/${ _pages[index] }');
+      if( index != _currentIndex ) {
+        html.window.history.pushState(null, 'none', '#/${ _pages[index] }');
+        _currentIndex = index;
+        html.document.title = _pages[index];
+      }
     });
 
   }  
@@ -31,7 +38,7 @@ class PageProvider extends ChangeNotifier {
   goTo( int index ) {
 
     // final routeName = _pages[index];
-    // html.window.history.pushState(null, 'none', '#/${ _pages[index] }');
+    html.window.history.pushState(null, 'none', '#/${ _pages[index] }');
 
     scrollController.animateToPage( 
       index, 
